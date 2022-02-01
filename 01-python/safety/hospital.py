e81b07960f8ba23263b1f9eb1fc454163e24f22a
unknown <zhuzg@bu.edu>
2022-02-01 14:33:54 -0500

    hospital

--- 01-python/safety/hospital.py
+++ 01-python/safety/hospital.py
@@ -1,3 +1,9 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
 def draw_hospital():
-    print("hospital not found")
-    return
+    img = mpimg.imread('hospital-image.jpg')
+    imgplot = plt.imshow(img)
+    plt.show()
+
+draw_hospital()
